50a80486a326b8bfa5fd3c352fa1bf0e20de86cf
Hugo Rischmann <hugo.rischmann@me.com>
2023-04-13 10:50:56 +0200

    Création du programme

---
+++ 00_journalists.rb
@@ -0,0 +1,137 @@
+$array_twitter = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+$choice = 0
+
+def write(str)
+  str.each_char do |char|
+    print char
+    sleep 0.03
+  end
+end
+
+def write_long(str)
+  str.each_char do |char|
+    print char
+    sleep 0.50
+  end
+end
+
+def clear_screen_short
+  system("clear")
+end
+
+def init
+  clear_screen_short
+  progress_bar = "█"
+  progress_bar_lenght = 81
+
+  write("Chargement en cours")
+  write_long("...\n")
+  write("#{progress_bar * progress_bar_lenght} 100%\n\n")
+  printf "██████████████████████████████████████████████████████████████████████████████████████\n"
+  printf "█████████████████ BIENVENUE SUR VOTRE ÉCRAN DE FONCTIONS SUR L'ARRAY██████████████████\n"
+  printf "██████████████████████████████████████████████████████████████████████████████████████\n"
+
+  printf "\n\n           Bienvenue sur le système de calcul et de recherche Gogolébontuyo ! \n\n
+  Voici les différentes options que nous pouvons vous proposer : \n"
+end
+
+def menu
+printf "
+  1. Calculer le nombre de valeurs contenues dans votre tableau.\n
+  2. Trouver la valeur la plus courte.\n
+  3. Trouver une valeur d'un nombre de caractères définis.\n
+  4. Connaître le nombre de valeurs commençant par une majuscule.\n
+  5. Trier la liste par ordre alphabétique.\n
+  6. Trier la liste par taille croissante.\n
+  7. Recherche une valeur.\n
+  8. Répartir les valeurs par nombre de caractères.\n
+  9. Quitter le programe.\n
+  > Votre choix : "
+  $choice = gets.chomp.to_i
+  while $choice < 1 || $choice > 9
+    printf "Merci de sélectionner une réponse valable :\n > Votre choix : "
+    $choice = gets.chomp.to_i
+  end
+  case $choice
+  when 1
+    length()
+  when 2
+    short()
+  when 3
+    searchbycar()
+  when 4
+    caps()
+  when 5
+    alph()
+  when 6
+    size()
+  when 7
+    look()
+  when 8
+    repartition()
+  when 9
+    quit()
+  end
+
+
+end
+
+def length
+  array_twitter_length = $array_twitter.length
+  puts "Le tableau contient #{array_twitter_length} valeurs."
+end
+
+def short
+  shortest_array_twitter = $array_twitter.min_by { |array_twitter| array_twitter.length }
+  puts "La valeur la plus courte est #{shortest_array_twitter}."
+end
+
+def searchbycar
+  puts "Combien de caractères voulez-vous dans la valeur ?"
+  car = gets.chomp.to_i
+  car_array_twitter = $array_twitter.count { |array_twitter| array_twitter.count("^@") == car }
+  puts "Il y a #{car_array_twitter} valeurs contenant #{car} caractères."
+end
+
+def caps
+  caps_array_twitter = $array_twitter.count { |array_twitter| array_twitter.match?(/^@[A-Z]/) }
+  puts "Il y a #{caps_array_twitter} valeurs commençant par une majuscule."
+end
+
+def alph
+  alph_array_twitter = $array_twitter.sort
+  puts "Liste des valeurs triées par ordre alphabétique : \n#{alph_array_twitter}."
+end
+
+def size
+  size_array_twitter = $array_twitter.sort_by { |array_twitter| array_twitter.length }
+  puts "Liste des valeurs triées par taille : \n #{size_array_twitter}"
+end
+
+def look
+  printf "Renseignez la valeur que vous recherchez :\n > "
+  search = gets.chomp.to_s
+  found = false
+  $array_twitter.each_with_index do |value, index|
+    if value.include?(search)
+      found = true
+      puts "Trouvé(e) ! La valeur '#{search}' est à la position numéro #{index} dans le tableau."
+    end
+  end
+  puts "Désolé mais '#{search}' n'a pas été trouvé(e) dans le tableau." unless found
+end
+
+def repartition
+  counts = $array_twitter.group_by(&:length).transform_values(&:count)
+  puts "Voici la répartition #{counts}"
+end
+
+def quit
+  printf "Merci d'avoir utilisé notre logiciel !"
+end
+
+init
+while $choice < 9
+  menu
+end
